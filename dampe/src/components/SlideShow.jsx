389d0c74b43a55c3069a71472c82dc98a800d1bb
Ulrika <u.sniedze@gmail.com>
2019-04-25 11:48:14 +0200

    Adds slideshow component

---
+++ dampe/src/components/SlideShow.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+export default class SlideShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentImageIndex: 0
+    };
+
+    this.nextSlide = this.nextSlide.bind(this);
+    this.previousSlide = this.previousSlide.bind(this);
+  }
+
+  previousSlide() {
+    const lastIndex = this.props.slides.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === 0;
+    const index = shouldResetIndex ? lastIndex : currentImageIndex - 1;
+
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  nextSlide() {
+    const lastIndex = this.props.slides.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === lastIndex;
+    const index = shouldResetIndex ? 0 : currentImageIndex + 1;
+
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  render() {
+    return (
+      <div className="carousel">
+        <Arrow
+          direction="left"
+          clickFunction={this.previousSlide}
+          glyph="&#9664;"
+        />
+        <img
+          className="image-slide"
+          src={this.props.slides[this.state.currentImageIndex].src}
+          alt=""
+        />
+        <Arrow
+          direction="right"
+          clickFunction={this.nextSlide}
+          glyph="&#9654;"
+        />
+      </div>
+    );
+  }
+}
+
+const Arrow = ({ direction, clickFunction, glyph }) => (
+  <div className={`slide-arrow ${direction}`} onClick={clickFunction}>
+    {glyph}
+  </div>
+);
